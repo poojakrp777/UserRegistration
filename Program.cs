5fc1903f631974ff052ad92908f5b3528fdb2238
poojakrp777 <poojakrp777@gmail.com>
2022-10-29 16:10:26 +0530

    validate first name

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration!...");
+
+            UserRegexPattern.FirstName();
+            Console.ReadLine();
+        }
+    }
+}
