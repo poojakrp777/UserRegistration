65078d1c68f9f12771ee7fce21bd2cf9128e59aa
poojakrp777 <poojakrp777@gmail.com>
2022-10-30 17:06:07 +0530

    Validating first name

--- UserRegexPattern.cs
+++ UserRegexPattern.cs
@@ -2,11 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace UserRegistration
 {
     internal class UserRegexPattern
     {
+        public static void FirstName()
+        {
+            string[] arr = { "Pooja", "pooja", "poo123", "POoja123" };
+            string pattern = "^[A-Z][a-z]{3}";
+
+            foreach (string input in arr)
+            {
+                if (Regex.IsMatch(input, pattern))
+                {
+                    Console.WriteLine("{0} is valid", input);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is Invalid", input);
+                }
+            }
+        }
     }
 }
